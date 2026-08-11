933abea3cb4b77add43328ff13ebcddc153f9b26
Nom-an <nomanvi123@gmail.com>
2026-08-11 15:38:56 +0600

    account number and pin validation for changing money value

--- home.js
+++ home.js
@@ -1,16 +1,39 @@
 
-
+const pin = 1234;
 document.getElementById('btn-add-money').addEventListener('click', function(e){
     e.preventDefault();
 
     const bankName = document.getElementById('bank-name').value;
     
-    const accountNumber = parseInt(document.getElementById('account-number').value);
+    const accountNumber = document.getElementById('account-number').value;
 
     const amountWithdraw = parseInt(document.getElementById('amount-withdraw').value);
 
     const pinNumber = parseInt(document.getElementById('pin-number').value);
 
-    console.log(bankName,accountNumber, amountWithdraw , pinNumber);
+    // console.log(bankName,accountNumber, amountWithdraw , pinNumber);
+
+    if(pin !== pinNumber){
+        alert("enter valid pin number");
+        return;
+
+    }
+
+
+    const amount = parseInt(document.getElementById('amount').innerText);
+        if(accountNumber.length < 11){
+        alert("Please enter valid account number");
+        return;
+    }
+
+
+    const newAmount = amount + amountWithdraw;
+
+    document.getElementById('amount').innerText= newAmount;
+    // console.log(newAmount)
+
+
+
+ 
   
 })
