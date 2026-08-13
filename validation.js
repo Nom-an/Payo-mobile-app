cadd44a35d9696a7435a977099ad8ebd4cbd0d98
Nom-an <nomanvi123@gmail.com>
2026-08-13 23:24:08 +0600

    type checking

--- validation.js
+++ validation.js
@@ -21,6 +21,15 @@ else{
 
 const numbers = [1,2,3,4,5];
 
+
 console.log(typeof numbers);
 
+console.log(Array.isArray(numbers));
 
+const student = {
+    name: 'noman',
+    location: 'Dhaka',
+}
+
+console.log(typeof student);
+console.log( student.name);
